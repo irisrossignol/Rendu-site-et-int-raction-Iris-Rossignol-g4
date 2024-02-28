81fb14b73ec65f3d736836e0e0821565f1a93c96
irisrossignol <iirisrossignol@gmail.com>
2024-02-29 00:20:49 +0100

    api

---
+++ test/api.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+function fetchHP(id) {
+
+    return fetch('https://hp-api.lainocs.fr/characters/' + id)
+    .then((response)=> response.json())
+}
+
+async function displayHP(id, elementId) {
+    const data = await fetchHP(id)
+    let date = data.birthday.slice(0,-14)
+    document.getElementById(elementId).innerHTML = `
+        <img src="${data.image}" alt="${data.name}"/>
+        <h1>Name : ${data.name}</h1>
+        <h1>Hairs : ${data.hairs}</h1>
+        <h1>Eye color : ${data.eyes}</h1>
+        <h1>Birthday : ${date}</h1>
+        <h1>Blood : ${data.blood}</h1>
+        <h1>Wand : ${data.wand}</h1>
+        <h1>Patronus : ${data.patronus}</h1>
+        <h1>Role : ${data.role}</h1>
+        <h1>House : ${data.house}</h1>
+        <h1>Actor : ${data.actor}</h1>
+
+` ;
+}
+
+displayHP('harry-potter', 'harry')
+displayHP('ron-weasley', 'ron')
+displayHP('draco-malfoy', 'draco')
+displayHP('hermione-granger', 'hermione')
+displayHP('albus-dumbledore', 'albus')
+displayHP('sirius-black', 'sirius')
+displayHP('cedric-diggory', 'cedric')
+displayHP('cho-chang', 'cho')
+displayHP('rubeus-hagrid', 'rubeus')
+displayHP('luna-lovegood', 'luna')
