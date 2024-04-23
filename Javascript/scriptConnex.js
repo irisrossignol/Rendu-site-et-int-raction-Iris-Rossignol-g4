6e882b00c808244797c7b468b654d57238da70cf
irisrossignol <iirisrossignol@gmail.com>
2024-04-23 23:20:13 +0200

    Login marche

--- Javascript/scriptConnex.js
+++ Javascript/scriptConnex.js
@@ -1,100 +1,6 @@
-        /*form = document.querySelector('form')
-        form.addEventListener("submit", async (event) => {
-            event.preventDefault();
-
-    const token = localStorage.getItem('token')
-    if (token) {
-        validToken(token)
-        .then((response) => {
-    if (response.ok) {
-        window.location.href = 'profil.html'
-    }
-        })
-        .catch((error) => {
-            console.log('error', error);
-        });
-    }
-
-    async function validToken(token) {
-        const response = await fetch('http://localhost:3000/auth/login', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-                
-        })
-        return
-    }
 
 
-    let response = await fetch('http://localhost:3000/auth/login', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            pseudo: pseudo,
-            password: password,
-        }),
-        })
-
-        let data = await response.json()
-            localStorage.setItem('token', data)
-        
-    })*/
-
-    
-    
-    
-    
-    
-    
-    /*form = document.querySelector('form')
-
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let pseudo = document.querySelector('#pseudo').value
-    let password = document.querySelector('#password').value
-
-    const data =  {
-        pseudo: pseudo,
-        password: password
-    }
-
-    console.log(JSON.stringify(data))
-
-    fetch("http://localhost:3000/auth/login", {
-      method: "POST", 
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data)
-    })
-    .then(res => res.json())
-    .then(data=>{
-        console.log(data)
-
-
-    })
-    .catch(e=> console.log(e))
-
-    });*/
-
-
-form.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    const validPassword = await bcrypt.compare(password, user.password)
-
-    if (!validPassword) {
-        return resizeBy.status(404).json({ error: 'Password not valid' })
-    }
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-    })
-    resizeBy.json(token)
-
+const login = async () => {
     let email = document.querySelector('input[name="email"]').value
     let password = document.querySelector('input[name="password"]').value
 
@@ -103,8 +9,6 @@ form.addEventListener("submit", async (event) => {
         headers: {
             'Content-Type': 'application/json',
         },
-    
-
         body: JSON.stringify({
             email: email,
             password: password,
@@ -112,9 +16,10 @@ form.addEventListener("submit", async (event) => {
     })
 
     let data = await response.json()
-    localStorage.setItem('token', data)
+    console.log(data)
+    localStorage.setItem('token', data.token)
 
     window.location.href = 'profil.html'
-        })
 
+}
 
